098b67256112c632ca6a8ab06b2786c6a020c292
pingineer-jp <pingineer@gmail.com>
2015-12-05 17:19:53 +0000

    change dir from /home/pi to /opt

--- etc/profile.d/tealion.sh
+++ etc/profile.d/tealion.sh
@@ -3,5 +3,5 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 export ALSADEV="pulse"
 export HOYA_API="API_KEY"
-export TEALION_ROOT="/home/pi/tealion"
+export TEALION_ROOT="/opt/tealion"
 /usr/sbin/alsactl --file ~/.config/asound.state restore
